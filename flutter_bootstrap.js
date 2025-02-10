42b14c0b0676dc34231e5283f728561440ae3770
FloRul <FloRul@users.noreply.github.com>
2025-02-10 15:28:10 +0000

    deploy: dfbb5dd15e9834edfb40ad2d0d7291ea83a838e8

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "109728702"
+    serviceWorkerVersion: "4289963861"
   }
 });
